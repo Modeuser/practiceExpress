7bf5c6f7ce2e0c3aeb8f9056a988c499b74641ac
modena <brian.usage@gmail.com>
2023-03-14 18:37:57 -0700

    prior to requiring html files

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    if (url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>home page</h1>')
+        res.end()
+    } else if (url == '/about'){
+        // return about
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>about page</h1>')
+        res.end()
+    } else {
+        // return 404
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write('<h1>page not found</h1>')
+        res.end()
+    }
+})
+
+server.listen(5012)
